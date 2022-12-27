0c0f635d22e0810f22474b7f12f8d6e95fff1d51
Steven Holley <mr.stevenholley@gmail.com>
2022-12-26 22:47:46 -0800

    Added exercises 1 & 2

---
+++ rb100/book_intro_to_programming_with_ruby/1.rb
@@ -0,0 +1,8 @@
+# Add two strings together that, when concatenated, return your first and last name as your full name in one string.	
+
+first_name = "Steven"
+last_name = "Holley"
+
+puts first_name + ' ' + last_name  #string concatenation 
+#puts "#{first_name} #{last_name}" #string interpolation
+ 
