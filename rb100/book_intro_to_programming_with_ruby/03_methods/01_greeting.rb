3fd362aab017c74ba11720521d243810588dca3d
Steven Holley <mr.stevenholley@gmail.com>
2023-01-08 00:44:10 +0000

    Added exercise files #1 & #2 in 03_Methods chapter.

---
+++ rb100/book_intro_to_programming_with_ruby/03_methods/01_greeting.rb
@@ -0,0 +1,8 @@
+#Write a program that prints a greeting message. 
+#It should contain a method called "greeting" that takes a name as a parameter & returns a string.
+
+def greeting(name)
+  "Hi, #{name}.  Nice to meet you!"
+end
+
+puts greeting('Steven')
