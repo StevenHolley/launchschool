5da21bc3627fa8c478c08cee641b5341b67048dc
Steven Holley <mr.stevenholley@gmail.com>
2023-01-25 15:01:41 +0000

    Updated solution for 3.rb to account for negative number user input.

--- rb100/book_intro_to_programming_with_ruby/05_loops/03.rb
+++ rb100/book_intro_to_programming_with_ruby/05_loops/03.rb
@@ -1,6 +1,6 @@
 #Write a method that counts down to zero using recursion.
 =begin
-#Concise, but didn't respond to negative numbers
+#Concise, but didn't output anything if user entered negative number
 def countdown(num)
   if num >= 0
     puts num
@@ -13,21 +13,10 @@ def countdown(num)
   if num <= 0
     puts num
   else
-    puts "The number #{num} is already less than 0."
+    puts num
+    countdown(num - 1)
   end 
 end
-
-=end
-=begin
-def count_to_zero(number)
-  if number <= 0
-    puts number
-  else
-    puts number
-    count_to_zero(number-1)
-  end
-end
-=end
 
 countdown(10)
 countdown(5)
